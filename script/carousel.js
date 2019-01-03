b411d5f0e3c43a718bd7b883538f3648c2fccb4f
YuraSadilenko <yura.sadilenko.it@gmail.com>
2019-01-03 19:57:36 +0200

    final version

---
+++ script/carousel.js
@@ -0,0 +1,89 @@
+(function () {
+  var carouselArr = [
+    {image: 'car.jpg', alt: 'Car'}, 
+    {image: 'apple.jpg', alt: 'Apple'}
+  ];
+  var newSlider = [
+    {image: 'peach.jpg', alt: 'peach'}, 
+    {image: 'pineapple.jpg', alt: 'pineapple'},
+    {image: 'tomato.jpg', alt: 'tomato'}
+  ];
+  var carouselIndex = 0;
+
+  console.log(nextSlide());
+  console.log(prevSlide());
+  console.log(switchSlide(1));
+  console.log(addItems(newSlider, 2));
+  console.log(removeItem(0));
+  console.log(getSliderInfo(0));
+  console.log(getSliderInfo(1));
+  console.log(getSliderInfo(2));
+  console.log(getSliderInfo(3));
+
+  function nextSlide() {
+    if (carouselIndex === (carouselArr.length - 1)) {
+      currentSlide = 0;
+      return carouselArr[currentSlide];
+    }
+    ++carouselIndex;
+    return carouselArr[carouselIndex];
+  };
+
+  function prevSlide() {
+    if(carouselIndex > 0) {
+      --carouselIndex;
+      return carouselArr[carouselIndex];
+    }
+    carouselIndex = carouselArr.length - 1;
+    return carouselArr[carouselIndex];
+  };
+
+  function switchSlide(n) {
+    if(n < carouselArr.length) {
+      carouselIndex = n;
+      return carouselArr[carouselIndex];
+    }
+    carouselIndex = carouselArr.length - 1;
+    return carouselArr[carouselIndex];
+  };
+
+  function addItems(obj, index) {
+    var arrIndex = index;
+    if (Array.isArray(obj)) {
+      for (var i = 0; i < obj.length; i++) {
+        addItems(obj[i], arrIndex);
+        ++arrIndex;
+      }
+    } else {
+      if (index <= carouselIndex) {
+        carouselArr.splice(arrIndex, 0, obj);
+        ++carouselIndex;
+      } else if (index < array.length) {
+        carouselArr.splice(arrIndex, 0, obj);
+      } else {
+        carouselArr.push(obj);
+      }
+    }
+    return carouselArr;
+  };
+
+  function removeItem(index) {
+    if (index <= carouselIndex) {
+      carouselArr.splice(index, 1);
+      --carouselIndex;
+    } else if (index < carouselArr.length) {
+      carouselArr.splice(index, 1);
+    } else {
+      console.log('Error');
+    }
+    return carouselArr;
+  };
+
+  function getSliderInfo(a) {
+    if (a < carouselArr.length) {
+      console.log(carouselArr[a].alt);
+    } else {
+      console.log('Error');
+    }
+  };
+})();
